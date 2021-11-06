5d0447f19803acc4e75f3bc5397cc27be1d7366f
Cristian <julchat@github.com>
2021-11-06 11:24:17 -0300

    lo que mostre

--- scala/src/main/scala/domain/Grupo.scala
+++ scala/src/main/scala/domain/Grupo.scala
@@ -1,5 +1,41 @@
 package domain
 
-class Grupo {
+case class Heroe (val saludActual : Int, val saludMaxima : Int, val nivelActual : Int = 1, val recursos : List[Recurso], trabajo : Trabajo) {
+  def subirDeNivel(): Heroe ={
+    trabajo.
+  }
+
+} //Debería ser una mónada para tener vivos y muertos? No creo que la nocion de un estado terminal valga la pena
+
+trait Trabajo{
+    modificar
+}
+
+case class Grupo (val heroes : List[Heroe], val cofreComun : Cofre){
+
 
 }
+
+case class Cofre(val items : List [Recurso], val armas : String, val tesoro : Int){
+
+}
+
+class Recurso //Puede ser: un item, un hechizo, una estadística, una habilidad
+
+
+object SimuladorDeGrupos{
+  def recorrerCalabozoConXNivelesAdicionales(cantidadNivelesExtra : Int, grupoDeHeroes : Grupo, desafio : Calabozo): Grupo = {
+    val heroesSubidos : List[Heroe] = grupoDeHeroes.heroes.map(heroe => heroe.subiDeNivel(cantidadNivelesExtra))
+    desafio.//fold?
+  }
+}
+
+class Calabozo(val habitaciones : List[Habitacion]){
+
+}
+
+class Habitacion(val puertas : List [Puerta], val dificultad : Situacion ){
+
+} //Pueden llegar a ser más de 4???
+
+//Son como las actividades las situaciones?
