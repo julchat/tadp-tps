307cf83ea5a1e7b1d9fef33a6624bafc0c3fd573
gabiperalta <gabiperalta16398@gmail.com>
2021-11-26 21:29:22 -0300

    termino con las puertas y sus dificultades

--- scala/src/main/scala/domain/Aventurero.scala
+++ scala/src/main/scala/domain/Aventurero.scala
@@ -1,12 +1,12 @@
 package domain
 
 case class Aventurero(atributos: Atributos, nivel:Int, salud: Int, trabajo: Trabajo){
+  require(salud > 0)
+
   def fuerza: Int = trabajo match {
     case Guerrero => atributos.fuerza + (atributos.fuerza * ((0.2 * nivel) + 1).toInt)
     case _ => atributos.fuerza
   }
-
-  def estaVivo: Boolean = salud > 0
 }
 
 case class Atributos(fuerza: Int,velocidad: Int)
@@ -14,13 +14,27 @@ case class Atributos(fuerza: Int,velocidad: Int)
 trait Trabajo
 case object Guerrero extends Trabajo
 case class Ladron(habilidad: Int) extends Trabajo
-case class Mago(hechizos: List[Hechizo]) extends Trabajo
+case class Mago(hechizos: List[HechizoTupla]) extends Trabajo
+
+case class HechizoTupla(hechizo: Hechizo, nivelRequerido: Int){
+  def conoceHechizo(hechizoAConocer: Hechizo, nivelActual: Int): Boolean = this.hechizo.equals(hechizoAConocer) && nivelActual >= nivelRequerido
+}
 
 trait Hechizo
-case class Invisibilidad(nivelRequerido: Int)
+case object Invisibilidad extends Hechizo
+case object Vislumbrar extends Hechizo
 
 
-case class Grupo(heroes: List[Aventurero], cofre: List[Item])
+case class Grupo(heroes: List[Aventurero], cofre: List[Item]){
+  def hayLadronConHabilidadOMas(listaHeroes:List[Trabajo] ,cantidadHabilidad: Int): Boolean = {
+    listaHeroes match {
+      case Ladron(habilidad) :: otrosHeroes => habilidad >= cantidadHabilidad
+      case _ :: otrosHeroes => hayLadronConHabilidadOMas(otrosHeroes,cantidadHabilidad)
+      case _ => false
+    }
+  }
+}
 
 trait Item
 case object Llave extends Item
+case object Ganzuas extends Item
--- scala/src/main/scala/domain/Calabozo.scala
+++ scala/src/main/scala/domain/Calabozo.scala
@@ -1,21 +1,57 @@
 package domain
 
+class ExecutionGrupoException(val grupo: Grupo) extends RuntimeException
+
 case class Calabozo(puertaPrincipal: Puerta){
-  def recorrer(grupo: Grupo): Grupo = puertaPrincipal.obstaculo match {
-    case Cerrada => grupo.cofre.find()
+  def recorrer(grupo: Grupo): Grupo = {
+    puertaPrincipal.abrir(grupo)
   }
 }
 
 case class Puerta(habitacion: Habitacion, obstaculo: Obstaculo){
-
+  def abrir(grupo: Grupo): Grupo = {
+    if (obstaculo(grupo)) habitacion.entrar(grupo)
+    else throw new ExecutionGrupoException(grupo)
+  }
 }
 
-trait Obstaculo
-case object Cerrada extends Obstaculo
-case object Escondida extends Obstaculo
-case object Encantada extends Obstaculo
+trait Obstaculo{
+  def apply(grupo: Grupo): Boolean = grupo.hayLadronConHabilidadOMas(grupo.heroes.map(heroe => heroe.trabajo),20) || sePuedeSuperar(grupo)
+  def sePuedeSuperar(grupo: Grupo): Boolean
+}
+
+case object Cerrada extends Obstaculo {
+  override def sePuedeSuperar(grupo: Grupo): Boolean = {
+    grupo.cofre.contains(Llave) || grupo.cofre.contains(Ganzuas) || grupo.hayLadronConHabilidadOMas(grupo.heroes.map(heroe => heroe.trabajo),10)
+  }
+}
+
+case object Escondida extends Obstaculo{
+  override def sePuedeSuperar(grupo: Grupo): Boolean = {
+    grupo.heroes.exists(heroe => {
+      heroe.trabajo match {
+        case Mago(hechizos) => hechizos.exists(hechizo => hechizo.conoceHechizo(Vislumbrar,heroe.nivel))
+        case Ladron(habilidad) => habilidad >= 6
+      }
+    })
+  }
+}
+
+case class Encantada(hechizoPuerta: Hechizo) extends Obstaculo{
+  override def sePuedeSuperar(grupo: Grupo): Boolean = {
+    grupo.heroes.exists(heroe => {
+      heroe.trabajo match {
+        case Mago(hechizos) => hechizos.exists(hechizo => hechizo.conoceHechizo(hechizoPuerta,heroe.nivel))
+      }
+    })
+  }
+}
 //case object Abierta extends EstadoPuerta
 
-case class Habitacion(situacion: Situacion, puertas: List[Puerta])
+case class Habitacion(situacion: Situacion, puertas: List[Puerta]){
+  def entrar(grupo: Grupo): Grupo = {
+    grupo.copy()
+  }
+}
 
 trait Situacion
