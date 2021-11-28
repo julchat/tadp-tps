3bc6ff7d3a09ec3842dc430e7887ff372dc2c28e
Sabrina Garcia <mvlts9819@gmail.com>
2021-11-28 02:21:23 -0300

    test funcan

--- scala/src/test/scala/domain/test.scala
+++ scala/src/test/scala/domain/test.scala
@@ -38,13 +38,13 @@ class test extends AnyFreeSpec {
       val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
       val mago = Vivo(Heroe(atributos, 2, 100, Mago(hechizos), Loquitos, Heroico))
 
-      var heroes=List[EstadoHeroe](guerrerouno,ladron,guerrerodos,mago)
+      var heroes = List[EstadoHeroe](guerrerouno, ladron, guerrerodos, mago)
 
-      var cofre=Cofre(List(Ganzúas,Llave),List("chuchillo","pistola"),45)
+      var cofre = Cofre(List(Ganzúas, Llave), List("chuchillo", "pistola"), 45)
       //var grupo=GrupoVivo(heroes,cofre,Habitacion((NoPasaNada,TrampaDeLeones),Puerta((NoPasaNada,TesoroPerdido),(Cerrada,Escondida))))
-      var grupo=GrupoVivo[EstadoHeroe](heroes,cofre,Habitacion(NoPasaNada,List.empty),List.empty)
+      var grupo = GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List.empty)
 
-      assert(grupo.cantidadDeVivos()==4)
+      assert(grupo.cantidadDeVivos() == 4)
     }
 
     "si una de las habitaciones tiene trampa de leones, muere el mas lento" in {
@@ -56,60 +56,83 @@ class test extends AnyFreeSpec {
       val guerrerodos = Vivo(Heroe(atributosMasLento, 2, 100, Guerrero, Loquitos, Heroico))
       val mago = Vivo(Heroe(atributos, 2, 100, Mago(hechizos), Loquitos, Heroico))
 
-      var heroes=List[EstadoHeroe](guerrerouno,ladron,guerrerodos,mago)
+      var heroes = List[EstadoHeroe](guerrerouno, ladron, guerrerodos, mago)
 
-      var cofre=Cofre(List(Ganzúas,Llave),List("chuchillo","pistola"),45)
+      var cofre = Cofre(List(Ganzúas, Llave), List("chuchillo", "pistola"), 45)
       //var grupo=GrupoVivo(heroes,cofre,Habitacion((NoPasaNada,TrampaDeLeones),Puerta((NoPasaNada,TesoroPerdido),(Cerrada,Escondida))))
-      var grupo=GrupoVivo[EstadoHeroe](heroes,cofre,Habitacion(NoPasaNada,List.empty),List.empty)
+      var grupo = GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List.empty)
 
-      val calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones,List.empty)),List.empty),Puerta(None,List.empty))
+      val calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List.empty), Puerta(None, List.empty))
 
       assert(calabozo.recorrer(grupo).cantidadDeVivos() == 3)
     }
     "el lider de un grupo es el primer miembro del grupo" in {
       var atributos = Atributos(100, 50, 80)
-      var heroes = List(Guerrero, Mago, Ladrón)
-      var grupo = GrupoVivo(heroes, Cofre, Habitacion((NoPasaNada, TrampaDeLeones), Puerta((NoPasaNada, TesoroPerdido), (Cerrada, Escondida))))
+      val ladron = Vivo(Heroe(atributos, 2, 100, Ladrón(30), Loquitos, Heroico))
+      val guerrerouno = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+      val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
 
-      assert(grupo.getLider() == Guerrero)
+      var heroes = List[EstadoHeroe](guerrerouno, guerrerodos, ladron)
+      var grupo = GrupoVivo[EstadoHeroe](heroes, Cofre(List.empty, List.empty, 30), Habitacion(NoPasaNada, List.empty), List.empty)
+
+      assert(grupo.getLider() == Some(guerrerouno))
     }
 
 
   }
   "Calabozo y puertas" - {
     "si una puerta esta CERRADA cualquier heroe puede abrirla si tiene el item llave" in {
-      val atributo = Atributos(100, 50, 80)
-      var heroes = List(Guerrero, Mago)
-      var cofre = Cofre((Llave), ("chuchillo,pistola"), 45)
-      var calabozo=Calabozo(Puerta(),Puerta(Habitacion,Habitacion,(Cerrada)))
-      var grupo = GrupoVivo(heroes, cofre, Habitacion((NoPasaNada, TrampaDeLeones), Puerta((NoPasaNada, TesoroPerdido), (Cerrada, Escondida))))
+      val atributos = Atributos(100, 50, 80)
+      val ladron = Vivo(Heroe(atributos, 2, 100, Ladrón(30), Loquitos, Heroico))
+      val guerrerouno = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+      val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+
+      var heroes = List[EstadoHeroe](guerrerouno, guerrerodos, ladron)
+      var cofre = Cofre(List(Llave), List("chuchillo,pistola"), 45)
+      var calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List.empty), Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Cerrada)))
+      var grupo = new GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Cerrada))))
+
       assert(calabozo.puertaPrincipal.puedoSerAbierta(grupo))
     }
     "si una puerta esta CERRADA cualquier cerradura puede ser abierta por un ladron si tiene habilidad mayor  igual que 10" +
       "o si el grupo tiene el item ganzuas" in {
-      val atributo = Atributos(100, 50, 80)
-      var heroes = List(Guerrero, Mago, Ladrón(20))
-      var cofre = Cofre((Ganzúas), ("chuchillo,pistola"), 45)
-      var calabozo=Calabozo(Puerta(),Puerta(Habitacion,Habitacion,(Cerrada)))
-      var grupo = GrupoVivo(heroes, cofre, Habitacion((NoPasaNada, TrampaDeLeones), Puerta((NoPasaNada, TesoroPerdido), (Cerrada, Escondida))))
+      val atributos = Atributos(100, 50, 80)
+      val ladron = Vivo(Heroe(atributos, 2, 100, Ladrón(30), Loquitos, Heroico))
+      val guerrerouno = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+      val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+
+      var heroes = List[EstadoHeroe](guerrerouno, guerrerodos, ladron)
+
+      var cofre = Cofre(List(Ganzúas), List("chuchillo,pistola"), 45)
+      var calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List.empty), Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Cerrada)))
+      var grupo = new GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Cerrada))))
 
       assert(calabozo.puertaPrincipal.puedoSerAbierta(grupo))
     }
     "si una puerta esta ESCONDIDA un mago puede encontrarla si conoce el hechizo vislumbrar" in {
-      val atributo = Atributos(100, 50, 80)
-      var heroes = List(Guerrero, Mago((Vislumbrar)))
-      var cofre = Cofre((Llave), ("chuchillo,pistola"), 45)
-      var calabozo=Calabozo(Puerta(),Puerta(Habitacion,Habitacion,(Escondida)))
-      var grupo = GrupoVivo(heroes, cofre, Habitacion((NoPasaNada, TrampaDeLeones), Puerta((NoPasaNada, TesoroPerdido), (Cerrada, Escondida))))
+      val atributos = Atributos(100, 50, 80)
+      var hechizos = List(HechizoAprendible(2, Vislumbrar))
+      val mago = Vivo(Heroe(atributos, 2, 100, Mago(hechizos), Loquitos, Heroico))
+      val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+      var heroes = List[EstadoHeroe](guerrerodos, mago)
+      var cofre = Cofre(List(Ganzúas), List("chuchillo,pistola"), 45)
+      var calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List.empty), Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Escondida)))
+      var grupo = new GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Escondida))))
+
       assert(calabozo.puertaSalida.puedoSerAbierta(grupo))
     }
     "si una puerta esta ESCONDIDA un ladron con 6 o mas de habilidad puede encontarla " in {
-      val atributo = Atributos(100, 50, 80)
-      var heroes = List(Guerrero, Ladrón(10))
-      var cofre = Cofre((Llave), ("chuchillo,pistola"), 45)
-      var calabozo=Calabozo(Puerta(),Puerta(Habitacion,Habitacion,(Escondida)))
-      var grupo = GrupoVivo(heroes, cofre, Habitacion((NoPasaNada, TrampaDeLeones), Puerta((NoPasaNada, TesoroPerdido), (Cerrada, Escondida))))
+      val atributos = Atributos(100, 50, 80)
+      val ladron = Vivo(Heroe(atributos, 2, 100, Ladrón(10), Loquitos, Heroico))
+      val guerrerodos = Vivo(Heroe(atributos, 2, 100, Guerrero, Loquitos, Heroico))
+      var heroes = List[EstadoHeroe](guerrerodos, ladron)
+      var cofre = Cofre(List(Ganzúas), List("chuchillo,pistola"), 45)
+      var calabozo = new Calabozo(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List.empty), Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Escondida)))
+      var grupo = new GrupoVivo[EstadoHeroe](heroes, cofre, Habitacion(NoPasaNada, List.empty), List(Puerta(Some(Habitacion(TrampaDeLeones, List.empty)), List(Escondida))))
+
       assert(calabozo.puertaSalida.puedoSerAbierta(grupo))
 
 
+    }
+  }
 }
